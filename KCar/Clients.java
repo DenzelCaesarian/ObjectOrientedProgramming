37a905e0e23be1f1dd51df4b96d717f36ef051b8
Denzel Caesarian Iswanto Suhardjo <101154087+DenzelCaesarian@users.noreply.github.com>
2022-04-03 09:10:26 +0700

    Create Clients.java

---
+++ KCar/Clients.java
@@ -0,0 +1,16 @@
+public class Clients {
+	String clientName;
+
+	public Clients(String clientName) {
+		super();
+		this.clientName = clientName;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+}
